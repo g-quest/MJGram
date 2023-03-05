bef7c408b279a1d6f866e485d8b04a514ff57fba
Greg Cuesta <gregory.cuesta@gmail.com>
2023-03-05 12:08:09 -0800

    Initial Commit

---
+++ MJGram/MJGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MJGramApp.swift
+//  MJGram
+//
+//  Created by GREGORY CUESTA on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MJGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
